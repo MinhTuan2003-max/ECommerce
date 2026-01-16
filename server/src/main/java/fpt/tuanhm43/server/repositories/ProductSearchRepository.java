97fa5fc7f6d90b3736ef9dfa469581232a7a93d8
Minh Tuấn <minhtuanha2829@gmail.com>
2026-01-16 13:58:17 +0700

    fix(email): refactor Thymeleaf email layout to proper fragment usage

--- server/src/main/java/fpt/tuanhm43/server/repositories/ProductSearchRepository.java
+++ server/src/main/java/fpt/tuanhm43/server/repositories/ProductSearchRepository.java
@@ -4,8 +4,6 @@ import fpt.tuanhm43.server.documents.ProductSearchDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface ProductSearchRepository extends ElasticsearchRepository<ProductSearchDocument, String> {
 }
